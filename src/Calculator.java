107a0883ca451fd0fcb5ebb9009f84bf4df3e3b6
Elias <elias.westrin@gmail.com>
2025-09-27 18:28:41 +0200

    Fungerande addition. Test i Main.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,35 @@
+import java.util.ArrayList;
+
 public class Calculator {
+
+    private ArrayList<Double> numbers = new ArrayList<Double>();
+    private char operator;
+
+    Calculator(ArrayList<Double> numbers, char operator)
+    {
+        this.numbers = numbers;
+        this.operator = operator;
+    }
+
+    public double calculate()
+    {
+        switch (operator)
+        {
+            case '+':
+                return add();
+            default:
+                throw new IllegalArgumentException("Ogiltig operator: " + operator);
+        }
+    }
+
+    public double add()
+    {
+        double sum = 0;
+
+        for (int i = 0; i < numbers.size(); i++)
+        {
+            sum += numbers.get(i);
+        }
+        return sum;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        /*=====================================
+        * PLACEHOLDER FÖR ATT TESTA FUNKTIONER
+        * =====================================*/
+        ArrayList<Double> numbers = new ArrayList<Double>();
+        double result;
+
+        //Lägger till tal i ArrayList
+        numbers.add(5.0);
+        numbers.add(-5.0);
+
+        char operator = '+';
+
+        Calculator calculator = new Calculator(numbers, operator);
+
+        result = calculator.calculate();
+
+        System.out.println(result);
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
